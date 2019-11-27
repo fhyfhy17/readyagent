12b35d1002f341181fdae70f528272e071ebd824
fenghaiyu <fenghaiyu@fotoable.com>
2019-11-27 20:50:34 +0800

    sgs的热更新

---
+++ src/main/java/com/util/FileUtil.java
@@ -0,0 +1,44 @@
+package com.util;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+
+    public static List<String> getFileNames(String filePath, String filter) {
+        File root = new File(filePath);
+        File[] files = root.listFiles(pathname -> {
+            String name = pathname.getName();
+            return name.endsWith(filter);
+        });
+        List<String> filelist = new ArrayList<>();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                getFiles(file.getAbsolutePath(), filter);
+                System.out.println("显示" + filePath + "下所有子目录及其文件" + file.getAbsolutePath());
+            } else {
+                filelist.add(file.getAbsolutePath());
+                System.out.println("显示" + filePath + "下所有子目录" + file.getAbsolutePath());
+            }
+        }
+        return filelist;
+    }
+    
+    public static List<File> getFiles(String filePath, String filter) {
+        File root = new File(filePath);
+        File[] files = root.listFiles(pathname -> {
+            String name = pathname.getName();
+            return name.endsWith(filter);
+        });
+        List<File> fileList = new ArrayList<>();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                getFiles(file.getAbsolutePath(), filter);
+            } else {
+                fileList.add(file);
+            }
+        }
+        return fileList;
+    }
+}
